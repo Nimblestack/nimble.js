58da15fe59afc4a570477e21e0420fcd55818ecd
clickslide <hello@clickslide.co>
2015-05-27 15:28:40 -0400

    Adding first set of files to nimble.js

---
+++ app-config.js
@@ -0,0 +1,11 @@
+// TODO: This should be the only file users have to edit.
+var config = {
+    appconfig: [
+        {url:"clickslide/yammersentimentanalysisbasic", sessid:null},
+        {url:"clickslide/ocrdocumentconcepts", sessid:null}
+    ],
+    initWithData: false,
+    startIndex: 0,
+    autoLogin: false, // if true, use machine based ID to login
+    local: true // if true, use localhost REMOVE FOR PRODUCTION
+};
